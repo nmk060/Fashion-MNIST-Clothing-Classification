665121060a9d45b8209c33358af613634970dc73
nmk060 <122669442+nmk060@users.noreply.github.com>
2023-03-30 22:18:54 +0530

    Update preprocessing.py

--- Baseline_Model/preprocessing.py
+++ Baseline_Model/preprocessing.py
@@ -1,3 +1,9 @@
+# convert from integers to floats
+train_norm = train.astype('float32')
+test_norm = test.astype('float32')
+# normalize to range 0-1
+train_norm = train_norm / 255.0
+test_norm = test_norm / 255.0
 # scale pixels
 def prep_pixels(train, test):
  # convert from integers to floats
